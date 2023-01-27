7b68f4d92084afaa7a8a04d523a61472862e1b89
NutiNaguti <derzhavin.danila@icloud.com>
2023-01-27 16:58:52 +0400

    added logging for input requests

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
@@ -16,9 +17,11 @@ require (
 )
 
 require (
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.4.0
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/rs/zerolog v1.29.0
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
--- main.go
+++ main.go
@@ -2,27 +2,42 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/NutiNaguti/night-bridge-indexer/common"
 	"github.com/NutiNaguti/night-bridge-indexer/db"
 	"github.com/NutiNaguti/night-bridge-indexer/handler"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog"
 )
 
 func main() {
 	// Load configs
 	conf := common.New()
 
+	// Create server
 	e := echo.New()
 
 	// Middleware
 	e.Pre(apiVersion)
-	// e.Use(middleware.BasicAuth())
-	// e.Use(middleware.CORS())
+	// TODO: replace default config by custom
+	e.Use(middleware.CORS())
+	// Setup logging
+	logger := zerolog.New(os.Stdout)
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:    true,
+		LogStatus: true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			logger.Info().Str("URI", v.URI).Int("status", v.Status).Msg("request")
+			return nil
+		},
+	},
+	))
+
+	e.GET("/", handler.GetInfo)
 
 	// Routings version 1.0
-	e.GET("/", handler.GetInfo)
 	e.GET("v1/tx/last", handler.GetLastTransaction)
 	e.GET("v1/tx", handler.GetTransactionsFromTo)
 	// Admin functions
@@ -41,7 +56,9 @@ func apiVersion(next echo.HandlerFunc) echo.HandlerFunc {
 		req := c.Request()
 		headers := req.Header
 		apiVer := headers.Get("version")
-		req.URL.Path = fmt.Sprintf("/%s%s", apiVer, req.URL.Path)
+		if apiVer != "" {
+			req.URL.Path = fmt.Sprintf("/%s%s", apiVer, req.URL.Path)
+		}
 		return next(c)
 	}
 }
