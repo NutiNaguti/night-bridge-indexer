9cac623fcd381113850b8c7bdfea2db5d2a5d8bd
NutiNaguti <derzhavin.danila@icloud.com>
2023-02-14 20:56:27 +0400

    added contexts in handler

--- handler/indexer.go
+++ handler/indexer.go
@@ -4,25 +4,37 @@ import (
 	"context"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/NutiNaguti/night-bridge-indexer/model"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
+	"golang.org/x/text/cases"
 )
 
+var defaultTimeDuration = time.Second * 10
+
 func GetLastTransaction(c echo.Context) error {
-	var tx *model.Transaction
-	tx, err := model.GetLastTransaction(context.Background())
-	if err != nil {
-		log.Error(err)
-		return err
+	d := time.Now().Add(defaultTimeDuration)
+	ctx, cancel := context.WithDeadline(context.Background(), d)
+	defer cancel()
+
+	tx := make(chan model.Transaction)
+	go model.GetLastTransaction(ctx, tx)
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case result := <-tx:
+		return c.JSON(http.StatusOK, result)
 	}
-	return c.JSON(http.StatusOK, tx)
 }
 
 func GetTransactionsFromTo(c echo.Context) error {
-	var txs model.TransactionsPage
-	var err error
+	d := time.Now().Add(defaultTimeDuration)
+	ctx, cancel := context.WithDeadline(context.Background(), d)
+	defer cancel()
+
 	timestamp := c.QueryParam("timestamp")
 	pageToken, err := strconv.ParseUint(c.QueryParam("page_token"), 10, 16)
 	if err != nil {
@@ -32,23 +44,35 @@ func GetTransactionsFromTo(c echo.Context) error {
 	if err != nil {
 		return err
 	}
-	txs, err = model.GetTransactionsSince(context.Background(), timestamp, uint16(pageToken), uint16(pageSize))
-	if err != nil {
-		log.Error(err)
-		return err
+
+	txs := make(chan model.TransactionsPage)
+	go model.GetTransactionsSince(ctx, timestamp, uint16(pageToken), uint16(pageSize), txs)
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case result := <-txs:
+		return c.JSON(http.StatusOK, result)
 	}
-	return c.JSON(http.StatusOK, txs)
 }
 
 func AddTransaction(c echo.Context) error {
+	d := time.Now().Add(defaultTimeDuration)
+	ctx, cancel := context.WithDeadline(context.Background(), d)
+	defer cancel()
+
 	sender := c.QueryParam("sender")
 	receiver := c.QueryParam("receiver")
 	amount := c.QueryParam("amount")
 	timestamp := c.QueryParam("timestamp")
-	err := model.CreateTransaction(context.Background(), sender, receiver, amount, timestamp)
-	if err != nil {
-		log.Error(err)
-		return err
+
+	success := make(chan bool)
+	go model.CreateTransaction(ctx, sender, receiver, amount, timestamp, success)
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case result := <-success:
+		return c.JSON(http.StatusOK, result)
 	}
-	return c.JSON(http.StatusOK, "")
 }
--- model/transaction.go
+++ model/transaction.go
@@ -2,9 +2,11 @@ package model
 
 import (
 	"context"
+	"time"
 
 	"github.com/NutiNaguti/night-bridge-indexer/db"
 	"github.com/jackc/pgx/v5"
+	"github.com/labstack/gommon/log"
 	"github.com/shopspring/decimal"
 )
 
@@ -23,23 +25,26 @@ type Transaction struct {
 	Timestamp int64           `json:"timestamp" db:"timestamp"`
 }
 
-func GetLastTransaction(ctx context.Context) (*Transaction, error) {
+func GetLastTransaction(ctx context.Context, out chan Transaction) error {
 	var tx Transaction
 	conn, err := db.CreateConnection()
 	if err != nil {
-		return &tx, err
+		log.Error(err)
+		return err
 	}
 
 	err = conn.QueryRow(ctx, `select * from transactions order by timestamp desc limit 1`).Scan(&tx.Id, &tx.Sender, &tx.Receiver, &tx.Amount, &tx.Timestamp)
 	conn.Close(ctx)
-
 	if err != nil {
-		return &tx, err
+		log.Error(err)
+		return err
 	}
-	return &tx, err
+
+	out <- tx
+	return err
 }
 
-func GetTransactionsSince(ctx context.Context, timestamp string, pageToken uint16, pageSize uint16) (TransactionsPage, error) {
+func GetTransactionsSince(ctx context.Context, timestamp string, pageToken uint16, pageSize uint16, out chan TransactionsPage) error {
 	if pageSize > maxPageSize {
 		pageSize = maxPageSize
 	}
@@ -49,28 +54,29 @@ func GetTransactionsSince(ctx context.Context, timestamp string, pageToken uint1
 
 	conn, err := db.CreateConnection()
 	if err != nil {
-		return txsResponse, err
+		return err
 	}
 
 	rows, _ := conn.Query(ctx, `select * from transactions where timestamp >= $1 order by timestamp limit $2 offset $3`, timestamp, pageSize, pageSize*(pageToken-1))
 	conn.Close(ctx)
 
 	if err != nil {
-		return txsResponse, err
+		return err
 	}
 
 	txs, err = pgx.CollectRows(rows, pgx.RowToStructByPos[Transaction])
 	if err != nil {
-		return txsResponse, err
+		return err
 	}
 
 	txsResponse.Txs = txs
 	txsResponse.NextPageToken = pageToken + 1
 
-	return txsResponse, err
+	out <- txsResponse
+	return err
 }
 
-func CreateTransaction(ctx context.Context, sender string, receiver string, amount string, timestamp string) error {
+func CreateTransaction(ctx context.Context, sender string, receiver string, amount string, timestamp string, out chan bool) error {
 	conn, err := db.CreateConnection()
 	if err != nil {
 		return err
@@ -82,5 +88,7 @@ func CreateTransaction(ctx context.Context, sender string, receiver string, amou
 	if err != nil {
 		return err
 	}
+
+	out <- true
 	return nil
 }
